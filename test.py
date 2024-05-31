4f5058693b7e438a555921ae0dd3fcf8e8697678
Dhananjaya Samanta Singhar <52064780+dhananjaysinghar@users.noreply.github.com>
2024-05-31 10:43:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,51 @@
 # C:\Users\Happy\AppData\Local\Microsoft\WindowsApps\python.exe
 
 
-import cv2
+
+# WAP to find the average
+# num1 = eval(input("Enter the 1st number : "))
+# num2 = eval(input("Enter the 2nd number : "))
+# num3 = eval(input("Enter the 3rd number : "))
+#
+# avg = round((num1 + num2 + num3) / 3, 2)
+#
+# print(f"the average of {num1} & {num2} & {num3} is {avg}")
 
 
-print(2)
+
+# WAP to find the tip % amount from bill.
+# billAmount = eval(input("Enter Bill Amount : "))
+# tipPercentage = eval(input("Tip % : "))
+#
+# tipAmount = (billAmount * tipPercentage) / 100
+# totalBill = billAmount + tipAmount
+#
+# print(round(totalBill, 2))
+
+
+# WAP to find the pounds from KG
+# weight = eval(input("Enter the weight : "))
+# pound = weight * 2.2
+# print(f"{weight} kg  = {round(pound,2)} pounds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
